952e9afa8335ce3b27affdaa74e2f8899954c173
Clyde <drericcity@gmail.com>
2021-01-26 23:00:13 +0900

    1st commit

---
+++ cmake-build-debug/CMakeFiles/Convert2Binary.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Convert2Binary.dir/main.c.obj"
+  "Convert2Binary.exe"
+  "Convert2Binary.exe.manifest"
+  "Convert2Binary.lib"
+  "Convert2Binary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Convert2Binary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
